3f09b23ab0cd94f637be6b325eea4e485e55b2f3
Slamchillz <mendyslam@gmail.com>
2022-09-15 03:15:20 +0000

    Bug fixes and test cases

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+
+"""
+This module defines the base class inherited by all other ORM classes
+"""
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,19 +12,22 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """
+    A base class for all hbnb models
+    """
     id = Column(String(60), unique=True, primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs:
-            del kwargs['__class__']
-            for k, v in kwargs:
+            if kwargs.get('__class__', None):
+                del kwargs['__class__']
+            for k, v in kwargs.items():
                 if k == 'created_at' or k == 'updated_at':
                     v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
                 self.__dict__.update({k: v})
@@ -29,7 +35,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
@@ -51,5 +57,8 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
+        attributes = {}
+        attributes.update(self.__dict__)
+        attributes.pop('_sa_instance_state', None)
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(cls, self.id, attributes)
