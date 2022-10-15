4096aec7e08b376449a37ee7b06038eb7f81a133
slamchillz <mendyslam@gmail.com>
2022-10-15 17:07:56 +0000

    utility: close file storage by calling reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -60,3 +60,9 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """
+        Calls reload() method
+        """
+        self.reload()
