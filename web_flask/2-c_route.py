c9403afce672f79b0189d6e0d5d7943763cf55d6
slamchillz <mendyslam@gmail.com>
2022-10-15 16:58:41 +0000

    flask basics

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application:
+Routes:
+    /:          display “Hello HBNB!”
+    /hbnb:      display “HBNB”
+    /c/<text>:  display “C ” followed by the value of the text variable
+                (replace underscore _ symbols with a space )
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """
+    Displays 'Hello HBNB!'
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+    Displays 'HBNB'
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
